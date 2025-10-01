a309bfa0b498e333db433a87aa9faed78aa0dec6
iLaimiK <1013069090@qq.com>
2025-10-01 19:14:47 +0800

    chore: update template from source

--- webpack.config.ts
+++ webpack.config.ts
@@ -344,6 +344,11 @@ function parse_configuration(entry: Entry): (_env: any, argv: any) => webpack.Co
           // globs: ['src/panel/component/*.vue'],
         }),
         new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
+        new webpack.DefinePlugin({
+          __VUE_OPTIONS_API__: false,
+          __VUE_PROD_DEVTOOLS__: process.env.CI !== 'true',
+          __VUE_PROD_HYDRATION_MISMATCH_DETAIL__: false
+        })
       )
       .concat(
         should_obfuscate
@@ -415,6 +420,9 @@ function parse_configuration(entry: Entry): (_env: any, argv: any) => webpack.Co
 
       const builtin = ['vue3-pixi', 'vue-demi'];
       if (builtin.includes(request)) {
+        return callback();
+      }
+      if (argv.mode !== 'production' && ['vue', 'pixi'].some(key => request.includes(key))) {
         return callback();
       }
       const global = {
